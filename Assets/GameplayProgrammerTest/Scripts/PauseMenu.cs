d7ea40764a21a67c3a831497d31b03e716fec3b8
Dood-x <Dood-x@users.noreply.github.com>
2022-09-07 22:44:11 +0200

    created a pause menu and pause menu script, binded buttons to activate and deactivate menues for navigation, ESC to pause/unpause

---
+++ Assets/GameplayProgrammerTest/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject PauseMenuCanvas;
+    private bool paused = false;
+
+    public void Pause()
+    {
+        PauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    public bool IsPaused()
+    {
+        return paused;
+    }
+
+    void Start()
+    {
+        Resume();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused())
+                Resume();
+            else
+                Pause();
+        }
+    }
+  
+
+    public void Exit()
+    {
+        Debug.Log("Quit");    
+        Application.Quit();
+    }
+
+}
